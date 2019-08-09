7b1b79b5a189e1fc12e267119b28496e47314992
Vanessa Corlette <nessacor16@gmail.com>
2019-08-09 02:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo(holiday,name)
+function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
 }
